f042f331b10996a2afa6c9177e8bdcbc0ded0495
lorio <lori@loriortiz.com>
2014-06-12 15:51:11 -0400

    Port project to MM, add Contact

--- config.rb
+++ config.rb
@@ -1,26 +1,79 @@
-# Require any additional compass plugins here.
+###
+# Compass
+###
 require "singularitygs"
 require 'toolkit'
-# Set this to the root of your project when deployed:
-http_path = "http//livemagnyc.com/www/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-fonts_dir = "fonts"
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
-# You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded #or :nested or :compact or :compressed
+###
+# Page options, layouts, aliases and proxies
+###
 
-# To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Reload the browser automatically whenever files change
+ activate :livereload
+
+# Methods defined in the helpers block are available in templates
+ helpers do
+  def page_title
+    'Page Title Base  '+ current_page.data.title
+  end
+  def page_id
+    current_page.data.title.downcase().tr(' ', '_')
+  end
+ end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :relative_links, true
 
 
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+   activate :minify_css
+
+  # Minify Javascript on build
+   activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+   activate :directory_indexes
+
+  # Use relative URLs
+   activate :relative_assets
+   activate :imageoptim
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
